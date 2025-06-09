c888431d39dee5a14b5ab60bb8401d37e2afaa6b
Jorge Raúl Rocha <147886215+roch21V2@users.noreply.github.com>
2025-06-08 22:37:56 -0600

    Update streamlit_app.py
    Adding subtitle and fixing request

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit+" "
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
       
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
